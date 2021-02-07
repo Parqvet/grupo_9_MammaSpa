8b16eca0dbaf7ea724597eb65a5212383f8eb18c
aliciamoro <56940460+aliciamoro@users.noreply.github.com>
2021-02-06 21:34:08 -0300

    prueba de Abm

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/indexRouter');
 var usersRouter = require('./routes/usersRouter');
-
+var adminRouter = require('./routes/adminRouter');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/admin', adminRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
