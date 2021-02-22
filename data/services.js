19977d1e397c5f9ef6569cbc90b345c29803deff
Marcelo <marcelosen4@gmail.com>
2021-02-22 11:53:13 -0300

    Se actualiza adminController y adminRouter para renderizar las vistas de los servicios

--- data/services.js
+++ data/services.js
@@ -3,10 +3,10 @@ const path = require('path');
 const services_db = path.join('data', 'services.json');
 
 module.exports = {
-    getProducts: () => {
+    getServices: () => {
         return JSON.parse(fs.readFileSync(services_db,'utf-8'));
     },
-    setProducts: (data) => {
+    setServices: (data) => {
         fs.writeFileSync(services_db, JSON.stringify(data, null, 2), 'utf-8');
     }
 }
