fdd63ebcebeaee7aa17a56d2800cf0f9eb4dcc19
aliciamoro <56940460+aliciamoro@users.noreply.github.com>
2021-04-29 11:24:34 -0300

    Se agrega vista dinamica a sevices en home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,12 +7,34 @@ module.exports = {
             order: Sequelize.literal('rand()'),
          
          })
-        .then(destacados => { 
-            return res.render('index', {
-                destacados
-            })
-        })
+               
+         const serviceOne = db.Services.findAll({
+            where : {
+                title : 'Facial Skin'
+            },
+            limit : 1,
+            order: Sequelize.literal('rand()'),
+           })
+       const serviceTwo = db.Services.findAll({
+           where : {
+               title : 'Manos Spa'
+           },
+           limit : 1,
+           order: Sequelize.literal('rand()'),
+           })
+
+       Promise.all([destacados,serviceOne,serviceTwo])
+       .then(([destacados,serviceOne,serviceTwo]) => { 
+           console.log(serviceTwo)
+           return res.render('index', {
+               destacados,
+               serviceOne : serviceOne[0],
+               serviceTwo : serviceTwo[0]
+           })
+       })
+         
         .catch( err => console.log(err))
+        
     },
 
     renderProductsMain: (req, res) => {
@@ -36,6 +58,7 @@ module.exports = {
 
 
     },
+    
 
     renderServicesMain: (req, res) => {
         db.Services.findAll()
